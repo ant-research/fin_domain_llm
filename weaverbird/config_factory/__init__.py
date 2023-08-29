44949920792caa1f0e33c065898251008a10f0ac
siqiao.xsq <siqiao.xsq@antgroup.com>
2023-08-29 20:43:32 +0800

    Add config factory

---
+++ weaverbird/config_factory/__init__.py
@@ -0,0 +1,7 @@
+from weaverbird.config_factory.basemodel_config import BaseModelConfig
+from weaverbird.config_factory.fintune_config import FinetuningConfig
+from weaverbird.config_factory.generation_config import GeneratingConfig
+
+__all__ = ['BaseModelConfig',
+           'GeneratingConfig',
+           'FinetuningConfig']
