39ecc5aeab21d95a0d679b9bd2e9d394d26d6ba0
siqiao.xsq <siqiao.xsq@antgroup.com>
2023-08-30 23:32:52 +0800

    Update __init__.py

--- weaverbird/utils/__init__.py
+++ weaverbird/utils/__init__.py
@@ -1,9 +1,14 @@
-from weaverbird.utils.kb_utils import get_kbs_list
 from weaverbird.utils.chatbot_utils import get_base_url, parse_text
-from weaverbird.utils.log_utils import default_logger as logger
 from weaverbird.utils.const import Language
+from weaverbird.utils.kb_utils import get_kbs_list
+from weaverbird.utils.log_utils import default_logger as logger
+from weaverbird.utils.misc import count_parameters, parse_configs
+from weaverbird.utils.registrable import Registrable
 
 __all__ = ['get_kbs_list',
            'get_base_url',
            'parse_text',
-           'Language']
+           'Language',
+           'Registrable',
+           'count_parameters',
+           'parse_configs']
