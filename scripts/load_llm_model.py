cdbef680e0ddcd10aafa57ebc092d2b7881e7430
siqiao.xsq <siqiao.xsq@antgroup.com>
2023-09-06 13:24:30 +0800

    update script

--- scripts/load_llm_model.py
+++ scripts/load_llm_model.py
@@ -3,7 +3,7 @@ from weaverbird.utils import parse_configs
 
 
 def main():
-    model_config_dict = {'model_name_or_dir': '/mnt/nas/users/antflux/LM/chatglm2-6b'}
+    model_config_dict = {'model_name_or_dir': 'chatglm2-6b'}
 
     model_config, _, _ = parse_configs(model_config_dict)
 
